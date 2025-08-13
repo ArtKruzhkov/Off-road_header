b08d02898e952123a6416dc5227148a90ce8f8c4
artem_kruzhkov <artkruzhkov@gmail.com>
2025-08-13 21:05:31 +0300

    Final before adaptive

---
+++ app/js/app.js
@@ -0,0 +1,113 @@
+import { gsap, Power2 } from 'gsap';
+import MicroModal from 'micromodal';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Modal
+
+	MicroModal.init({
+		openTrigger: 'data-micromodal-open',
+		closeTrigger: 'data-micromodal-close',
+		disableFocus: true,
+		disableScroll: true,
+		awaitOpenAnimation: true,
+		awaitCloseAnimation: true,
+		onClose: () => {
+			document.activeElement.blur();
+		}
+	})
+
+
+	// Swiper
+
+	const swiperImg = new Swiper('.slider-img', {
+		parallax: true,
+		speed: 2500,
+		loop: false,
+		mousewheel: true,
+		pagination: {
+			el: '.slider-pagination-count .total',
+			type: 'custom',
+			renderCustom: function (swiper, current, total) {
+				let totalRes = total >= 10 ? total : `0${total}`;
+				return totalRes;
+			}
+		}
+	});
+	const swiperText = new Swiper('.slider-text', {
+		loop: false,
+		speed: 2500,
+		mousewheel: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true
+		},
+		navigation: {
+			prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next'
+		}
+	});
+
+	swiperText.controller.control = swiperImg;
+	swiperImg.controller.control = swiperText;
+
+	// Gear
+
+	const sliderGear = document.querySelector('.slider-gear');
+
+	swiperText.on('slideNextTransitionStart', function () {
+		gsap.to(sliderGear, {
+			duration: 2.8,
+			rotation: '+=45',
+			ease: Power2.easeOut
+		});
+	});
+
+	swiperText.on('slidePrevTransitionStart', function () {
+		gsap.to(sliderGear, {
+			duration: 2.8,
+			rotation: '-=45',
+			ease: Power2.easeOut
+		});
+	});
+
+	// Slide Change
+
+	const currentNum = document.querySelector('.slider-pagination-count .current');
+	const currentNumBottom = document.querySelector('.slider-pagination-current__num');
+
+	swiperText.on('slideChange', function () {
+		let index = swiperText.realIndex + 1,
+			indexRes = index >= 10 ? index : `0${index}`;
+
+		gsap.to(currentNum, {
+			duration: 0.2,
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			ease: Power2.easeOut,
+			onComplete: function () {
+				gsap.to(currentNum, {
+					duration: 0.1,
+					force3D: true,
+					y: 10
+				})
+				currentNum.innerHTML = indexRes
+				currentNumBottom.innerHTML = indexRes
+			}
+		})
+		gsap.to(currentNum, {
+			duration: 0.2,
+			force3D: true,
+			y: 0,
+			opacity: 1,
+			ease: Power2.easeOut,
+			delay: 0.3
+		})
+	})
+
+});
